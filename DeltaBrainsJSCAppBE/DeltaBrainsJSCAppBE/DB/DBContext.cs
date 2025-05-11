bc4f9eeedd54b0e62ca1ce32e7e0d922d04e9c80
DangLH <ledang01637@gmail.com>
2025-05-11 14:21:58 +0700

    Init

---
+++ DeltaBrainsJSCAppBE/DeltaBrainsJSCAppBE/DB/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DeltaBrainJSC.DB
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options)
+        : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+        }
+
+
+
+    }
+}
